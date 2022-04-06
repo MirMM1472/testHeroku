6a6c35a341041a21b6321fba6cc90d7215a445f2
MirMM1472 <meermazhar@hztech.biz>
2022-04-07 00:43:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/players", (req, res) => {
+  res.send(data);
+});
+app.listen(port, () => {
+  console.log(`LMS backend is listningon port http://localhost:${port}`);
+});
